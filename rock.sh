4717edc3791e742a05a423714c176eddb4a604e2
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-10-30 19:28:28 +0800

    Update rock.sh

--- rock.sh
+++ rock.sh
@@ -4,6 +4,7 @@ chmod 777 dero
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6hsk03skqgr8tscz0xla -r 139.162.40.79:80 -p stratum;
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6hsk03skqgr8tscz0xla -r 139.162.40.79:80 -p rpc;
     sleep 5;
 done
+
