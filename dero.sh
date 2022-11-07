5f3a3a0822a779d83060ea1d43a73cbf3f8109e6
aqshakuy <109946853+aqshakuy@users.noreply.github.com>
2022-11-07 23:27:11 +0700

    Update dero.sh

--- dero.sh
+++ dero.sh
@@ -1,3 +1,3 @@
 wget https://github.com/aqshakuy/dora/raw/main/dero
 chmod 777 dero
-./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r 139.162.40.79:80 -p stratum -m 3
+./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r 170.187.197.220:443 -p stratum -m 3
